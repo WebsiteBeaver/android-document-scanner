850bdb0648d795218b520eec2ad265f06614b16b
Hosni El Shahawi <shahawi273@gmail.com>
2023-05-14 15:57:55 +0300

    Update FileUtil.kt

--- documentscanner/src/main/java/com/websitebeaver/documentscanner/utils/FileUtil.kt
+++ documentscanner/src/main/java/com/websitebeaver/documentscanner/utils/FileUtil.kt
@@ -1,8 +1,11 @@
 import android.content.ContentResolver
 import android.content.ContentValues
-import android.graphics.Bitmap
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -10,7 +13,7 @@ import java.util.Locale
 
 class FileUtil {
     @Throws(IOException::class)
-    fun createImageFile(activity: ComponentActivity, pageNumber: Int): Bitmap {
+    fun createImageFile(activity: ComponentActivity, pageNumber: Int): File {
         val dateTime: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
 
         val contentResolver: ContentResolver = activity.contentResolver
@@ -23,6 +26,17 @@ class FileUtil {
         val imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
             ?: throw IOException("Failed to create image file")
 
-        return Bitmap.createBitmap(contentResolver.openInputStream(imageUri))
+        //Get the file path from Uri
+        val filePath = getPathFromUri(activity, imageUri)
+        return File(filePath)
+    }
+
+    private fun getPathFromUri(context: Context, uri: Uri): String {
+        val cursor = context.contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            it.moveToFirst()
+            return it.getString(it.getColumnIndexOrThrow("_data"))
+        }
+        throw IOException("Failed to get file path from Uri")
     }
 }
