b6191f28f896f4c01f379b64bfaac8c475e9c624
dmarcs <marcusdavid127@gmail.com>
2023-06-03 19:04:45 -0400

    fix cvt color error

--- documentscanner/src/main/java/com/websitebeaver/documentscanner/utils/ImageUtil.kt
+++ documentscanner/src/main/java/com/websitebeaver/documentscanner/utils/ImageUtil.kt
@@ -3,10 +3,13 @@ package com.websitebeaver.documentscanner.utils
 import android.content.ContentResolver
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import com.websitebeaver.documentscanner.extensions.distance
 import com.websitebeaver.documentscanner.extensions.toOpenCVPoint
 import com.websitebeaver.documentscanner.models.Quad
+import java.io.File
 import kotlin.math.min
 import org.opencv.android.Utils
 import org.opencv.core.Mat
@@ -23,6 +26,56 @@ import org.opencv.imgproc.Imgproc
  */
 class ImageUtil {
     /**
+     * get image matrix from file path
+     *
+     * @param filePath image is saved here
+     * @return image matrix
+     */
+    private fun getImageMatrixFromFilePath(filePath: String): Mat {
+        // read image as matrix using OpenCV
+        val image: Mat = Imgcodecs.imread(filePath)
+
+        // if OpenCV fails to read the image then it's empty
+        if (!image.empty()) {
+            // convert image to RGB color space since OpenCV reads it using BGR color space
+            Imgproc.cvtColor(image, image, Imgproc.COLOR_BGR2RGB)
+            return image
+        }
+
+        if (!File(filePath).exists()) {
+            throw Exception("File doesn't exist - $filePath")
+        }
+
+        if (!File(filePath).canRead()) {
+            throw Exception("You don't have permission to read $filePath")
+        }
+
+        // try reading image without OpenCV
+        var imageBitmap = BitmapFactory.decodeFile(filePath)
+        val rotation = when (ExifInterface(filePath).getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_NORMAL
+        )) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> 90
+            ExifInterface.ORIENTATION_ROTATE_180 -> 180
+            ExifInterface.ORIENTATION_ROTATE_270 -> 270
+            else -> 0
+        }
+        imageBitmap = Bitmap.createBitmap(
+            imageBitmap,
+            0,
+            0,
+            imageBitmap.width,
+            imageBitmap.height,
+            Matrix().apply { postRotate(rotation.toFloat()) },
+            true
+        )
+        Utils.bitmapToMat(imageBitmap, image)
+
+        return image
+    }
+
+    /**
      * get bitmap image from file path
      *
      * @param filePath image is saved here
@@ -30,10 +83,7 @@ class ImageUtil {
      */
     fun getImageFromFilePath(filePath: String): Bitmap {
         // read image as matrix using OpenCV
-        val image: Mat = Imgcodecs.imread(filePath)
-
-        // convert image to RGB color space since OpenCV reads it using BGR color space
-        Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2BGR)
+        val image: Mat = this.getImageMatrixFromFilePath(filePath)
 
         // convert image matrix to bitmap
         val bitmap = Bitmap.createBitmap(image.cols(), image.rows(), Bitmap.Config.ARGB_8888)
@@ -51,10 +101,7 @@ class ImageUtil {
      */
     fun crop(photoFilePath: String, corners: Quad): Bitmap {
         // read image with OpenCV
-        val image = Imgcodecs.imread(photoFilePath)
-
-        // convert image to RGB color space since OpenCV reads it using BGR color space
-        Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2BGR)
+        val image = this.getImageMatrixFromFilePath(photoFilePath)
 
         // convert top left, top right, bottom right, and bottom left document corners from
         // Android points to OpenCV points
